b1eea8f8957a2e740b6519bc80a258b5a8df49e6
i阿--松 <9469724+a-song123@user.noreply.gitee.com>
2021-08-18 04:13:17 +0000

    update day8-18-1.cpp. 在探索那个中括号内，限定的数字少一，然后他减去输入变量则可以得到的那个倒序的数字的奥秘

--- day8-18-1.cpp
+++ day8-18-1.cpp
@@ -6,17 +6,17 @@ int main(void)
 	int x[7];
 
 	for (i = 0; i < 7; i++) {
-		printf("x[%d]:", i);
-		scanf_s("%d", &x[i]);
+		printf("x[%d]:", i);  //输入编码 
+		scanf_s("%d", &x[i]); //为了输入自己设置的变量
 	}
 
 	for (i = 0; i < 3; i++) {
-		int tmp = x[i];
-		x[i] = x[6 - i];
-		x[6 - i] = tmp;
+		int tmp = x[i];     // 引入新的中界值
+		x[i] = x[6 - i];    // 这样只能倒换一半
+		x[6 - i] = tmp;     // 倒换另一半
 	}
 
-	puts("倒序排列了。");
+	puts("倒序排列了。");    //后续的工作和之前的代码是一样的。
 	for (i = 0; i < 7; i++)
 		printf("x[%d] = %d\n", i, x[i]);
 	return 0;
